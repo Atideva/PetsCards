2195d3556e92ca33f933c9ae8bcd6dbde55afa47
Eduard Kik <eduard.kik.ig@gmail.com>
2022-09-13 00:48:54 +0500

    Analytics - leve start removed from main menu

--- Assets/Scripts/Analytics/OnLevelStart.cs
+++ Assets/Scripts/Analytics/OnLevelStart.cs
@@ -1,17 +1,24 @@
+using Analytics;
 using Firebase.Analytics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Analytics
+namespace Analitics
 {
     public class OnLevelStart : MonoBehaviour
     {
+        int _sceneIndex;
+        string _sceneName;
+
         void Start()
         {
             var activeScene = SceneManager.GetActiveScene();
             var sceneName = activeScene.name;
             var level = new Parameter(Params.LEVEL_NAME, sceneName);
+        
             FirebaseAnalytics.LogEvent(EventName.LEVEL_START, level);
         }
+
+    
     }
 }
